0b3c44f30a04df67e4501cd6e2370c95815ce9f7
Falke666 <sokolovmslu@gmail.com>
2025-11-19 18:20:27 +0300

    HW

--- lesson6/homework/if_6.py
+++ lesson6/homework/if_6.py
@@ -5,3 +5,58 @@
 3 - вывести True если  одна пара переменных является целочисленным типом. 
     Пары могут образовать только следующие переменные - a1-a3, a2-a4, a3-a4"
 """
+
+a1 = 1.5
+a2 = 2
+a3 = "hello"
+a4 = 10.0
+
+float1 = type(a1) == float
+float2 = type(a2) == float
+float3 = type(a3) == float
+float4 = type(a4) == float
+
+all_floats = float1
+if all_floats:
+    all_floats = float2
+if all_floats:
+    all_floats = float3
+if all_floats:
+    all_floats = float4
+
+print("Все дробные:", all_floats)
+
+str1 = type(a1) == str
+str2 = type(a2) == str
+str3 = type(a3) == str
+str4 = type(a4) == str
+
+one_string = False
+if str1:
+    one_string = True
+if not one_string and str2:
+    one_string = True
+if not one_string and str3:
+    one_string = True
+if not one_string and str4:
+    one_string = True
+
+print("Есть строка:", one_string)
+
+int1 = type(a1) == int
+int2 = type(a2) == int
+int3 = type(a3) == int
+int4 = type(a4) == int
+
+pair_exists = False
+
+if int1 and int3:
+    pair_exists = True
+
+if not pair_exists and int2 and int4:
+    pair_exists = True
+
+if not pair_exists and int3 and int4:
+    pair_exists = True
+
+print("Есть пара целочисленных:", pair_exists)
