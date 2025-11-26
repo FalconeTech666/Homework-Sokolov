888718cfb67c43a94bc98c5860e6311b019014fb
Falke666 <sokolovmslu@gmail.com>
2025-11-26 16:09:50 +0300

    HW

--- lesson8/homework/5.py
+++ lesson8/homework/5.py
@@ -5,4 +5,15 @@
 Нельзя пользоваться collections.Counter!
 
 '''
+def count_char(text):
+    result = {}                    
 
+    for char in text:              # в цикле делаю логику - если буква впервые в списке - то вход 1, если нет, то вход +1
+        if char in result:         
+            result[char] += 1      
+        else:                      
+            result[char] = 1       
+
+    return result  
+
+print(count_char("привет"))
--- lesson8/homework/6.py
+++ lesson8/homework/6.py
@@ -7,4 +7,21 @@ Yes - если число уже встречалось и No, если нет
 если в списке не все целые числа вернуть False.
 
 """
+def yes_or_no(nums):
+    for n in nums:
+        if not isinstance(n, int): # все ли элементы - целые числа
+            return False
 
+    seen = set()      # сюда встреченные числа
+    result = []       # сюда ответы "yes"/"no"
+
+    for n in nums:
+        if n in seen:
+            result.append("yes")
+        else:
+            result.append("no")
+            seen.add(n)
+
+    return result
+
+print(yes_or_no([1, 2, 3, 1, 4]))
