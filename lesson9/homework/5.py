160a432952ebf5211d1510c419b35995d19bdb8d
Falke666 <sokolovmslu@gmail.com>
2025-12-02 20:44:01 +0300

    HW

--- lesson9/homework/5.py
+++ lesson9/homework/5.py
@@ -33,7 +33,28 @@
 ----11
 12
 
-
-
-
 '''
+
+# Рекурсивная функция
+
+def print_nested(lst, indent=0, symbol="--"):
+    for element in lst:
+        if isinstance(element, list):
+            print_nested(element, indent+1, symbol)
+        else:
+            print(symbol * indent + str(element))
+
+print_nested([1, 2, 3, [4, [5, 6], 7], 8, 9])
+
+# Функция без рекурсии
+
+def print_nested_iter(lst, symbol="--"):
+    stack = [(lst, 0)]
+    while stack:
+        element, indent = stack.pop()
+        if isinstance(element, list):
+            for child in reversed(element):
+                stack.append((child, indent + 1))
+        else:
+           print(symbol * indent + str(element))
+print_nested_iter([1,[2,[[3],4]],5,[[[6,7]]],8,[[[[9,10]],11]],12])
