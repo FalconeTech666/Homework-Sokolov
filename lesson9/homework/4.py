124e03c7798335214d6e9695273a12c872d952f4
Falke666 <sokolovmslu@gmail.com>
2025-12-02 20:08:07 +0300

    HW

--- lesson9/homework/4.py
+++ lesson9/homework/4.py
@@ -9,3 +9,18 @@
 
 Использовать map и lambda.
 '''
+
+nums = [1,2,3,4,5,6,7,8,9]
+
+nums1 = nums.copy()
+nums2 = nums.copy()
+nums3 = nums.copy()
+
+nums1 = list(map(lambda x: x**2, nums1))
+print(nums1)
+
+nums2 = list(map(lambda x: x+3 if x%2==0 else x, nums2))
+print(nums2)
+
+nums3 = list(map(lambda x: x*2 if x%2==0 else x*3, nums3))
+print(nums3)
