d680bc5ab6aaf7b38ed08e6e1e564448a57b12cf
Falke666 <sokolovmslu@gmail.com>
2025-12-10 14:18:05 +0300

    HW

--- lesson12/homework/2.py
+++ lesson12/homework/2.py
@@ -12,3 +12,45 @@
 
 Аксессоры реализоваться через property.
 """
+class BookCard:
+    def __init__(self, author, title, year):
+        self.author = author
+        self.title = title
+        self.year = year
+
+    @property
+    def author(self):
+        return self.__author
+
+    @author.setter
+    def author(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Author must be str")
+        self.__author = value
+
+    @property
+    def year(self):
+        return self.__year
+
+    @year.setter
+    def year(self, value):
+        if not isinstance(value, int):
+            raise ValueError("Year must be int")
+        if value <= 0 or value > 2025: 
+            raise ValueError("Invalid year")
+        self.__year = value
+
+    def __lt__(self, other):
+        return self.year < other.year
+
+    def __eq__(self, other):
+        return self.year == other.year
+    
+b1 = BookCard("Толстой", "Война и мир", 1873)
+b2 = BookCard("Оруэлл", "1984", 1949)
+b3 = BookCard("Роулинг", "Гарри Поттер", 1997)
+
+books = [b1, b2, b3]
+
+for b in sorted(books):
+    print(b.__dict__)
